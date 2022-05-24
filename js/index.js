298d991c98474176ab56030bd6b154aea075102b
coliTNE <tinellijuan7@gmail.com>
2022-05-24 18:58:49 -0300

    first commit

--- js/index.js
+++ js/index.js
@@ -1,185 +1,253 @@
-document.addEventListener("scroll", function(){
-    let header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0)})
 
-//Simulador de Interes Plazo Fijo
+//Declaracion de Variables Principales
 
-const planes = [
-    {
-        "meses" : 12,
-        "interes" : 2,
-    },
-    {
-        "meses" : 18,
-        "interes" : 4,
-    },
-    {
-        "meses" : 24,
-        "interes" : 6,
-    },
-    {
-        "meses" : 18,
-        "interes" : 3,
-    },
-    {
-        "meses" : 20,
-        "interes" : 5,
-    },
-    {
-        "meses" : 14,
-        "interes" : 3,
-    },
-    {
-        "meses" : 24,
-        "interes" : 5,
-    },
-    {
-        "meses" : 30,
-        "interes" : 7,
-    }
-]
+let planes = []
 
-let step = -1
+fetch('http://localhost:5501/js/planes.json')
+    .then((response) => response.json())
+    .then((json) => planes = planes.concat(json))
+
+let nombre
+
+let apellido
+
+let pasos = -1
 
 let dinero = 0
 
 let int = 0
- 
-let month = 0
 
-let labels = document.querySelectorAll(".main__content")
+let mes = 0
 
-let selectedMonth
+let diapositivas = document.querySelectorAll(".main__content")
 
-let selectedInt
+let mesElegido
 
-console.log(labels)
+let interesElegido
 
-let intCards = document.querySelector(".main__intCards").querySelectorAll("div");
-console.log(intCards)
+let reset = document.getElementById('reset')
 
-intCards.forEach(element => {
-    element.addEventListener("click", function(){
-        intCards.forEach(card=>card.classList.remove("activeInt"))
+// Boton Reset
+
+reset.onclick = () => {
+    Swal.fire({
+        title: 'Estas seguro de volver al inicio?',
+        text: "Se perderan todos tus datos!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#f58331',
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Si, volver al inicio!',
+        allowOutsideClick: false,
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let lastStep = document.getElementById("main__divBtn")
+            lastStep.classList.remove("hidden")
+            let planesContenedor = document.getElementById("opcionesPlan")
+            planesContenedor.innerHTML = ""
+            reset.classList.add("hidden")
+            anteriorPaso()
+        }
+    })
+}
+
+// visualizacion de boton de reset
+
+const checkReset = () => {
+    if (pasos > 0) {
+        reset.classList.remove("hidden")
+    }
+}
+
+// Activador De Cartas
+
+let tarjetasInteres = document.querySelector(".main__tarjetasInteres").querySelectorAll("div");
+
+tarjetasInteres.forEach(element => {
+    element.addEventListener("click", function () {
+        tarjetasInteres.forEach(card => card.classList.remove("activeInt"))
         this.classList.add("activeInt")
     })
 });
 
-let mesCards = document.querySelector(".main__mesCards").querySelectorAll("div");
-console.log(mesCards)
+let tarjetasMeses = document.querySelector(".main__tarjetasMeses").querySelectorAll("div");
 
-mesCards.forEach(element => {
-    element.addEventListener("click", function(){
-        mesCards.forEach(card=>card.classList.remove("activeMes"))
+tarjetasMeses.forEach(element => {
+    element.addEventListener("click", function () {
+        tarjetasMeses.forEach(card => card.classList.remove("activeMes"))
         this.classList.add("activeMes")
     })
 });
 
-function showNextStep(){
-    step++
-    console.log(step)
-    for (label of labels) label.className = "hidden"
-    labels[step].className = "";
+
+//Visibilidad de los pasos
+
+function siguientePaso() {
+    pasos++
+    console.log(pasos)
+    for (pagina of diapositivas) pagina.className = "hidden"
+    diapositivas[pasos].className = "";
+    checkReset()
 }
 
+function anteriorPaso() {
+    pasos = 0
+    console.log(pasos)
+    for (pagina of diapositivas) pagina.className = "hidden"
+    diapositivas[pasos].className = "";
+    checkReset()
+}
+
+// Estructura Principal del Simulador interactivo
+
 let btn = document.getElementById('btn')
-btn.addEventListener("click", function() {
-    if (step == 0) {
+btn.addEventListener("click", function () {
+    if (pasos == 0) {
+        nombre = document.getElementById('nombre').value
+        apellido = document.getElementById('apellido').value
         dinero = parseFloat(document.getElementById('monto').value)
-        showNextStep()
+
+        // se declara variable para checkear localStorage
+
+        let datosLocales = JSON.parse(localStorage.getItem('planUsuario'))
+        console.log(datosLocales)
+
+        //Validar si el usuario tiene un plan guardado anteriormente
+        if (isNaN(parseFloat(dinero)) || parseFloat(dinero) < 1) {
+            Swal.fire({
+                title: 'Ingrese una cantidad valida de dinero',
+                confirmButtonColor: '#f58331',
+            })
+            anteriorPaso()
+        } else if (nombre === "" || apellido === "") {
+            Swal.fire({
+                title: 'Complete todos sus datos porfavor',
+                confirmButtonColor: '#f58331',
+            })
+            anteriorPaso()
+        } else if (datosLocales != null && nombre === datosLocales.nombre && apellido === datosLocales.apellido) {
+                Swal.fire({
+                    title: 'Ya tienes un plan en nuestra pagina!',
+                    text: "Quieres cargarlo?",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#f58331',
+                    cancelButtonText: 'No, Gracias',
+                    confirmButtonText: 'Si, porfavor!',
+                    allowOutsideClick: false
+                }).then((result) => {
+    
+                    // Se muestra datos de su plan
+    
+                    if (result.isConfirmed) {
+                        Swal.fire({
+                            title: `${datosLocales.nombre} ${datosLocales.apellido}`,
+                            text: ` Usted ingreso $${datosLocales.montoInicial} y generó $${datosLocales.ganancia.toFixed(1)} de ganancia !`,
+                            confirmButtonColor: '#f58331',
+                        })
+                    } else {
+                        siguientePaso()
+                    }
+                })
+            
+        } else {
+            siguientePaso()
+        }
     }
-    else if (step == 1) {
-        let findInt = document.getElementsByClassName('activeInt');
-        let foundInt = findInt[0].innerText.slice(0, -1);
-        int = parseInt(foundInt)
+    else if (pasos == 1) {
+
+        // declaracion y asignacion de cartas de Interes
+
+        let buscarInteres = document.getElementsByClassName('activeInt');
+        let interesEncontrado = buscarInteres[0].innerText.slice(0, -1);
+        int = parseInt(interesEncontrado)
         console.log(int)
-        showNextStep()
+        siguientePaso()
     }
-    else if (step == 2) {
-        let findMonth = document.getElementsByClassName("activeMes");
-        let foundMonth = findMonth[0].innerText;
-        month = parseFloat(foundMonth)
-        console.log(month)
-        calculate()
-        let planCards = document.querySelectorAll(".main__optionCard");
-        console.log(planCards)
-        planCards.forEach(element => {
-        element.addEventListener("click", function(){
-        planCards.forEach(card=>card.classList.remove("activePlan"))
-        this.classList.add("activePlan")
+    else if (pasos == 2) {
+
+        // declaracion y asignacion de cartas de Meses
+        let buscarMes = document.getElementsByClassName("activeMes");
+        let mesEncontrado = buscarMes[0].innerText;
+        mes = parseFloat(mesEncontrado)
+
+        console.log(mes)
+
+        // se llama a la funcion calculadora con el plan de interes y de meses seleccionados anteriormente
+
+        calcular()
+
+        // se declara los planes disponibles
+
+        let cartasPlanes = document.querySelectorAll(".main__optionCard");
+        console.log(cartasPlanes)
+
+        // se elige el plan activo
+
+        cartasPlanes.forEach(element => {
+            element.addEventListener("click", function () {
+                cartasPlanes.forEach(card => card.classList.remove("activePlan"))
+                this.classList.add("activePlan")
 
             })
         });
-        findFinalMonth = document.querySelector(".activePlan .monthData")
-        selectedMonth = findFinalMonth.innerText
-        findFinalInt = document.querySelector(".activePlan .intData")
-        selectedInt = findFinalInt.innerText.slice(0, -1);
-        showNextStep()
+        siguientePaso()
     }
-    else if (step == 3) {
+    else if (pasos == 3) {
+        finalPlan()
 
-        console.log(selectedMonth)
-        console.log(selectedInt)
+        //Creacion de cuadro de texto final
+
+        let finalMsg = document.getElementById("result")
+        let finalUsuario = document.getElementById("usuario")
+        const [mensaje, saldosF, localUsuario] = finalPlan()
+        finalUsuario.innerHTML = `<p>Felicitaciones ${nombre} ${apellido} !</p>`
+        finalMsg.appendChild(mensaje)
+
+        //Boton para visualizar los saldos
+
+        let saldosBtn = document.getElementById("finalSaldos")
+        saldosBtn.onclick = () => {
+            Swal.fire(saldosF)
+        }
+
+        //se guarda el plan con todos los datos relevantes
+
+        localStorage.setItem('planUsuario', JSON.stringify(localUsuario))
+        siguientePaso()
+
+        //se esconde el boton principal de navegacion
         let lastStep = document.getElementById("main__divBtn")
         lastStep.className = "hidden"
-
     }
 })
 
-showNextStep()
+siguientePaso()
 
-
-function calculate(){
+function calcular() {
     // Aca se busca planes que cumplan con alguna de las dos condiciones
 
-    let planElegido = planes.filter( (elemento)=> elemento.interes == int || elemento.meses == month)
+    let planElegido = planes.filter((elemento) => elemento.interes == int || elemento.meses == mes)
 
     console.log(planElegido)
 
-    function plazoFijo (monto,interes,meses) {
-        let saldos = []
-        if (isNaN(parseFloat(monto)) || parseFloat(monto) < 0) {
-            alert("Ingrese una cantidad valida de dinero")
-            return
-        } else if 
-            (isNaN(parseFloat(interes)) || parseFloat(interes) < 0) { 
-                alert("Ingrese una tasa de interes valida")
-            return
-        } else if 
-            (isNaN(parseFloat(meses)) || parseFloat(meses) < 0) { 
-            alert("Ingrese una cantidad valida de meses")
-            return
-        } else {
-            for (let i = 1; i <= meses; i++) {
-                monto += interes*monto/100
-                saldos.push(monto.toFixed(1))
-            }
-        }
-        return saldos
-    }
-
-    planElegido.forEach((el)=> {
-        let s = plazoFijo(dinero,el.interes, el.meses)
-        let g = s.slice(-1)[0] - dinero
-        console.log(g.toFixed(2))    
-        el.ganacia = g
-        el.montoInicial = dinero
-        el.saldos = s
-    })
+    //se crean las cartas de los planes filtrados
 
     let planesContenedor = document.getElementById("opcionesPlan")
     let main__optionCards = document.createElement("div")
     main__optionCards.className = "main__optionCards"
     planesContenedor.appendChild(main__optionCards);
 
-    for ([index,planesElegidos] of planElegido.entries()) {
+    for ([index, planesElegidos] of planElegido.entries()) {
         let opcionPlan = document.createElement("div")
-        opcionPlan.className= "main__optionCard"
-  
+        opcionPlan.className = "main__optionCard"
+
         opcionPlan.innerHTML = `<h3>Plan Nro ${index + 1}</h3>
                                 <div class="cardBox">
                                     <div>
-                                        <p class="monthData">${planesElegidos.meses}</p>
+                                        <p class="mesData">${planesElegidos.meses}</p>
                                         <p>meses</p>
                                     </div>
                                     <div>
@@ -189,23 +257,72 @@ function calculate(){
                                 </div>`
         main__optionCards.appendChild(opcionPlan);
     }
+}
 
-    //Aca se elije  automaticamente el plan con mas ganancia
+function finalPlan() {
 
-    planElegido.sort((a,b) => b.ganacia - a.ganacia)
+    //se filtra el plan seleccionado
+
+    findFinalMonth = document.querySelector(".activePlan .mesData")
+    mesElegido = findFinalMonth.innerText
+    findFinalInt = document.querySelector(".activePlan .intData")
+    interesElegido = findFinalInt.innerText.slice(0, -1);
+
+    //se calculan los saldos correspondientes al plan
+
+    function plazoFijo(monto, interes, meses) {
+        let saldos = []
+        let sum = 0
+        for (let i = 1; i <= meses; i++) {
+            monto += interes * monto / 100
+            saldos.push(monto.toFixed(1))
+        }
+        for (let index = 0; index < saldos.length; index++) {
+            sum += parseFloat(saldos[index])
+        }
+        console.log(sum)
+        return saldos
+    }
+
+    let saldosFinal = plazoFijo(dinero, interesElegido, mesElegido);
+
+    //creacion de objeto con los datos relevantes
+
+    let planFinal = {
+        nombre: nombre,
+        apellido: apellido,
+        interes: int,
+        meses: mes,
+        saldos: saldosFinal,
+        montoInicial: dinero,
+        ganancia: saldosFinal.slice(-1)[0] - dinero
+    }
+
+    //creacion del mensaje final hacia el usuario
+
+    let mensaje = document.createElement("div")
+    mensaje.className = "main__mensajeFinal"
+    mensaje.innerHTML = `<p>Su saldo Final es de $ ${planFinal.saldos.slice(-1)[0]} y Usted Genero $ ${(planFinal.saldos.slice(-1)[0] - dinero).toFixed(2)} de ganancia</p>`
+
+    console.log(planFinal)
+
+    //creacion de cuadro de texto con todos los saldos ordenados
 
     msg = ""
 
-    for (let index = 0; index < planElegido[0].saldos.length; index++) {
-        const element = planElegido[0].saldos[index];
-        msg += "Saldo Mes Nro " + (index + 1) + " $ " + element + "\n"
+    for (let index = 0; index < planFinal.saldos.length; index++) {
+        const element = planFinal.saldos[index];
+        msg += "Mes Nro " + (index + 1) + " $ " + element + "\n"
     }
 
-    alert(msg)
+    return [mensaje, msg, planFinal]
 
-    let finalMsg = document.getElementById("result")
-    let mensaje = document.createElement("div")
-    mensaje.innerHTML = `<p>Su saldo Final es de $ ${planElegido[0].saldos.slice(-1)[0]} y Usted Genero $ ${planElegido[0].saldos.slice(-1)[0] - dinero.toFixed(2)} de ganancia</p>`
-    finalMsg.appendChild(mensaje)
-    return planElegido
 }
+
+
+//Animacion Header
+
+document.addEventListener("scroll", function () {
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
